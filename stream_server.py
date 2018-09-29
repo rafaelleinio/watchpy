359f8cb8828b1367fc6f4a6b8e7df707d1ed267c
Unknown <rafael.leper@gmail.com>
2018-09-29 17:18:07 -0300

    updating

--- stream_server.py
+++ stream_server.py
@@ -8,24 +8,23 @@ from pygame.locals import *
 
 
 def server():
-    try:
-    	print "\nServer started at " + str(socket.gethostbyname(socket.gethostname())) + " at port " + str(90)	
-    	port = 90
-    	serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    	serversocket.bind(("",port))
-    	serversocket.listen(10)
-    	pygame.camera.init()
-    	cam = pygame.camera.Camera(0,(640,480),"RGB")
-    	cam.start()
-    	img = pygame.Surface((640,480))
-    	while True:
-    		connection, address = serversocket.accept()
-    		print "GOT_CONNECTION"
-    		cam.get_image(img)
-    		data = pygame.image.tostring(img,"RGB")
-    		connection.sendall(data)
-    		connection.close()
-    except:
-    	pass
+    port = 8045
+    host = '0.0.0.0'
+    print("\nServer started at " + str(socket.gethostbyname(socket.gethostname())) + " at port " + str(port))
+    serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    serversocket.bind(("", port))
+    serversocket.listen(10)
+    pygame.camera.init()
+    cam = pygame.camera.Camera("/dev/video0", (640,480), "RGB")
+    cam.start()
+    img = pygame.Surface((640, 480))
+    while True:
+        print("Ready to connect")
+        connection, address = serversocket.accept()
+        print("GOT_CONNECTION")
+        cam.get_image(img)
+        data = pygame.image.tostring(img, "RGB")
+        connection.sendall(data)
+        connection.close()
 
 server()
